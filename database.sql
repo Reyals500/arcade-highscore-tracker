f7e3c84004f0be8dd2a6e9b28a96b1ee1ad46e78
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2024-02-05 15:27:57 -0600

    Added in a gmae router, currently setting it up, added database set up stuff

--- database.sql
+++ database.sql
@@ -2,8 +2,33 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+-- CREATE TABLE "user" (
+--     "id" SERIAL PRIMARY KEY,
+--     "username" VARCHAR (80) UNIQUE NOT NULL,
+--     "password" VARCHAR (1000) NOT NULL
+-- );
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+  "id" SERIAL PRIMARY KEY,
+  "username" VARCHAR(80) UNIQUE NOT NULL,
+  "password" VARCHAR(1000) NOT NULL,
+  "email" TEXT,
+  "phone_number" TEXT,
+  "region" TEXT
+);
+
+CREATE TABLE "Game_table" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(80) NOT NULL,
+  "img_url" VARCHAR(200),
+  "overview_text" TEXT NOT NULL
+);
+
+CREATE TABLE "Leaderboard" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER NOT NULL REFERENCES "user" ("id"),
+  "game_id" INTEGER NOT NULL REFERENCES "Game_table" ("id"),
+  "scores" TEXT,
+  "img" TEXT,
+  "date" DATE,
+  "time" TIME
 );
