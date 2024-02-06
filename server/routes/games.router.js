1e87d100ec09a37ed93bcfba17be2710a3167222
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2024-02-06 09:37:21 -0600

    GET games works

--- server/routes/games.router.js
+++ server/routes/games.router.js
@@ -1,5 +1,22 @@
 const express = require('express');
-const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
+const userStrategy = require('../strategies/user.strategy');
+const router = express.Router();
 
-router.get
+router.get('/', rejectUnauthenticated , (req, res) => {
+    console.log('/games GET route');
+    console.log('is authenticated?', req.isAuthenticated());
+    console.log('user', req.user);
+    let queryText = `SELECT * FROM "Game_table"`
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500)
+    })
+})
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route Includes
 const userRouter = require('./routes/user.router');
+const gamesRouter = require('./routes/games.router')
 
 // Express Middleware
 app.use(express.json());
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 // Routes
 app.use('/api/user', userRouter);
+app.use('/api/games', gamesRouter)
 
 // Listen Server & Port
 app.listen(PORT, () => {
